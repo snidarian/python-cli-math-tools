079d1e999f537e202edd00cfbcf4538ef9ec3821
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-01-24 13:08:16 -0500

    added degree option feature to --verbose flag

--- sqr.py
+++ sqr.py
@@ -1,20 +1,23 @@
 #! /usr/local/bin/python3.9
-# change above shebang line to reflect absolute path to preffered python interpreter
+# change above shebang line to reflect absolute path to preferred python interpreter
 
 import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("square", help="Squares provided number", type=int)
-parser.add_argument("-v", "--verbose", help="verbose output", action="store_true")
+parser.add_argument("square", type=int, help="display square of given number")
+parser.add_argument("-v", "--verbose", type=int, help="increase output verbosity", choices=[0, 1, 2])
 
 args = parser.parse_args()
 answer = args.square**2
 
-if args.verbose:
-  print(f"{args.square} squared is equal to {answer}")
+if args.verbose == 2:
+    print(f"The square of {args.square} is {answer}")
+elif args.verbose == 1:
+    print(f"{args.square}^2 == {answer}")
 else:
-  print(args.square**2)
+    print(answer)
+
 
 
 
