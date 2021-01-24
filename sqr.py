57fed7f106a30d50990577b86f4690c41575a652
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-01-24 11:03:07 -0500

    added verbose single and double flag feature

--- sqr.py
+++ sqr.py
@@ -1,15 +1,20 @@
 #! /usr/local/bin/python3.9
+# change above shebang line to reflect absolute path to preffered python interpreter
 
 import argparse
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("square", help="Squares provided number", type=int)
+parser.add_argument("-v", "--verbose", help="verbose output", action="store_true")
 
 args = parser.parse_args()
+answer = args.square**2
 
-
-print(args.square**2)
+if args.verbose:
+  print(f"{args.square} squared is equal to {answer}")
+else:
+  print(args.square**2)
 
 
 
